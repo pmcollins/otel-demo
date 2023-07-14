bd424582556ef00aed1a84227b5e8a41ce70e7f2
Pablo Collins <pcollins@splunk.com>
2023-07-14 15:34:04 -0700

    tweak urls

--- desktop/admin.py
+++ desktop/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 
-from .models import NumberDataPoint
+from .models import NumberDataPoint, Resource, ResourceAttribute, ScopeMetrics, Metric, ScalarMetric
 
+admin.site.register(Resource)
+admin.site.register(ResourceAttribute)
+admin.site.register(ScopeMetrics)
+admin.site.register(Metric)
+admin.site.register(ScalarMetric)
 admin.site.register(NumberDataPoint)
--- otel_demo/urls.py
+++ otel_demo/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('desktop/', include('desktop.urls')),
+    path('', include('desktop.urls')),
     path('admin/', admin.site.urls),
 ]
